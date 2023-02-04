be180ef57f17a76b3304ca2dcf01ca4c727263a7
Pierre808 <pierre.equit@gmail.com>
2023-02-04 15:05:14 +0100

    faster anim. on leihgabe

--- app/Views/Leihgabe/leihgabeErstellen.php
+++ app/Views/Leihgabe/leihgabeErstellen.php
@@ -78,7 +78,7 @@
                 success.innerHTML += 
                 '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /> <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /> </svg>';
 
-                await new Promise(resolve => setTimeout(resolve, 1800));
+                await new Promise(resolve => setTimeout(resolve, 1100));
 
                 window.location.href = "<?= base_url("add-gegenstand-to-leihgabe/") ?>/" + code;
             }
